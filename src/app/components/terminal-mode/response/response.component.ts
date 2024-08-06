452cf44291c05296b678d088065c3bc57e13a16e
eric-sclafani <eric.sclafani321@gmail.com>
2024-08-06 08:21:59 -0400

    improved help command

--- src/app/components/terminal-mode/response/response.component.ts
+++ src/app/components/terminal-mode/response/response.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
 import { Command } from '../../../interfaces/terminal';
 
 @Component({
 	selector: 'app-response',
 	standalone: true,
-	imports: [],
+	imports: [NgTemplateOutlet],
 	templateUrl: './response.component.html',
 	styleUrl: './response.component.scss'
 })
@@ -17,4 +18,21 @@ export class ResponseComponent {
 	@Input() links: any;
 
 	public funnyVideoLink = localStorage['funny'];
+
+	public helpCommands:Record<string,string>[] = [
+		{ title: 'welcome', text: 'welcome message'},
+		{ title: 'help', text: 'see all commands' },
+		{ title: 'hello', text: 'a very friendly greeting' },
+		{ title: 'about', text: 'About me' },
+		{ title: 'projects', text: 'noteworthy projects' },
+		{ title: 'contact', text: 'contact information' },
+		{ title: 'resume', text: 'my resume (PDF, new tab)' },
+		{ title: 'github', text: 'my github account (new tab)' },
+		{ title: 'linkedin', text: 'my linkedin (new tab)' },
+		{ title: 'repo', text: 'repo for this website (new tab)' },
+		{ title: 'funny', text: 'opens a random video I find funny (new tab)' },
+		{ title: 'clear or cl', text: 'clears terminal history' },
+		{ title: 'text', text: 'switches to \'text\' mode' },
+		{ title: 'ericspasswords', text: 'all of my juicy passwords at your disposal' },
+	]
 }
